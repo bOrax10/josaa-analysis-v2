2f013ee510b8946e5463ddb8f91394d36648b325
bOrax10 <atharvadixit104@gmail.com>
2024-07-02 15:33:19 +0530

    changed path for fetching

--- josaa-analysis/src/ParseBranch.js
+++ josaa-analysis/src/ParseBranch.js
@@ -63,7 +63,7 @@ class CsvTable extends Component {
             maxRankParam +
             pageNumberParam;
 
-        fetch(`http://localhost:5000/get_csv_by_branch${filterParam}`)
+        fetch(`http://borax10.pythonanywhere.com/get_csv_by_branch${filterParam}`)
             .then((response) => response.json())
             .then((data) => {
                 const totalRows = data.total_rows ? data.total_rows : 0;
